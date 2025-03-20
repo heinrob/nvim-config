40ec7586597bb6d0a7f5be71df01f0a36c2e2c59
heinrob <github@petrosh.de>
2025-03-20 08:58:17 +0100

    More cleanup, add neogit

--- lua/custom/plugins/treesitter.lua
+++ lua/custom/plugins/treesitter.lua
@@ -19,6 +19,7 @@ return { -- Highlight, edit, and navigate code
       'scss',
       'typoscript',
       'typescript',
+      'javascript',
     },
     -- Autoinstall languages that are not installed
     auto_install = true,
